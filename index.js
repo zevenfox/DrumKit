2a2b14865bfabb2ab5037069cb89c9189108eabc
kollawat rupanya <micky2smart1@gmail.com>
2023-01-24 17:39:28 +0700

    add button animation

--- index.js
+++ index.js
@@ -4,42 +4,17 @@ var audio = new Audio("sounds/tom-1.mp3")
 for(i=0; i< numberOfDrumBotton; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
     var buttonInnerHTML = this.innerHTML;
-    switch(buttonInnerHTML){
-        case "w":
-            var audio = new Audio("sounds/tom-1.mp3")
-            audio.play();
-            break;
-        case "a":
-            var audio = new Audio("sounds/tom-2.mp3")
-            audio.play();
-            break;
-        case "s":
-            var audio = new Audio("sounds/tom-3.mp3")
-            audio.play();
-            break;
-        case "d":
-            var audio = new Audio("sounds/tom-4.mp3")
-            audio.play();
-            break;
-        case "j":
-            var audio = new Audio("sounds/crash.mp3")
-            audio.play();
-            break;
-        case "k":
-            var audio = new Audio("sounds/kick-bass.mp3")
-            audio.play();
-            break;
-        case "l":
-            var audio = new Audio("sounds/snare.mp3")
-            audio.play();
-            break;
-        default: console.log(buttonInnerHTML);
-    }
+    playSound(buttonInnerHTML);
+    buttonAnimation(buttoninnerHTML)
+
+
+
     })
 } 
 
-addEventListener("keydown", function(event){
+document.addEventListener("keydown", function(event){
     playSound(event.key)
+    buttonAnimation(event.key)
 });
 
 function playSound(key){
@@ -76,6 +51,14 @@ function playSound(key){
     }
 }
 
+
+function buttonAnimation(currentKey){
+    var activeBotton = document.querySelector("." + currentKey)
+    activeBotton.classList.add("pressed");
+    setTimeout(function (){
+        activeBotton.classList.remove("pressed")
+    }, 100);
+}
 
 
 //or
